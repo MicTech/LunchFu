b4fc5ad1c42cfe03ab6904e60fe509f774a11e9e
Martin Bednar <mbednar@vendavo.com>
2013-04-20 13:27:03 +0200

    Removing meal Display filtered items

--- client/client.js
+++ client/client.js
@@ -5,6 +5,7 @@ Meteor.Router.add({
 
         Session.set('orderId', id);
 
+
         return 'order';
     },
 
@@ -21,17 +22,32 @@ Template.createNewOrder.events({"click #startOrdering": function (evnt, template
         hashCode: hashCode,
         restaurantUrl: restaurantUrl,
         endTime: endTime,
-        emailGroup: emailGroup});
+        emailGroup: emailGroup,
+        meals: []
+    });
 
     Meteor.Router.to('/order/' + id)
 }});
 
 Template.order.order = function () {
-    return Orders.findOne(Session.get('orderId'));
+    var mealId = amplify.store(getMealIdName());
+    var order = Orders.findOne(Session.get('orderId'));
+
+    if (!order) return;
+
+    var filteredOrders = order.meals.filter(function (it) {
+        return it.id != mealId;
+    });
+
+    var myOrder = order.meals.filter(function (it) {
+        return it.id == mealId;
+    });
+
+    return {order: order, filteredOrders: filteredOrders, myOrder: myOrder.length == 0 ? null : myOrder[0]};
 };
 
-Template.order.canOrderMeal = function() {
-    return Session.get('mealId')==undefined;
+Template.order.canOrderMeal = function () {
+    return amplify.store(getMealIdName()) == undefined;
 }
 
 Template.order.events({
@@ -42,5 +58,17 @@ Template.order.events({
         var id = Session.get('orderId');
         Orders.update({ _id: id }, {$push: {meals: {id: mealId, email: email, meal: meal}}});
         Session.set('mealId', mealId);
+
+        amplify.store(getMealIdName(), mealId);
+    },
+    'click #remove': function (evnt, template) {
+        var mealId = amplify.store(getMealIdName());
+        var id = Session.get('orderId');
+        Orders.update({ _id: id }, {$pull: {meals: {id: mealId}}});
+        amplify.store(getMealIdName(), null);
     }
 })
+
+function getMealIdName() {
+    return Session.get('orderId') + '_mealId';
+}
