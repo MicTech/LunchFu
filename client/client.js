6b9ee718f4b6c4dc4a639ec362f7d54b34ac852e
Michal Tehnik <michal.tehnik@mictech.cz>
2013-04-20 15:22:59 +0200

    Grouping of meal list and added Gravatar loading

--- client/client.js
+++ client/client.js
@@ -2,9 +2,7 @@ Meteor.Router.add({
     '/': 'createNewOrder',
 
     '/order/:id': function (id) {
-
         Session.set('orderId', id);
-
 
         return 'order';
     },
@@ -35,40 +33,81 @@ Template.order.order = function () {
 
     if (!order) return;
 
-    var filteredOrders = order.meals.filter(function (it) {
-        return it.id != mealId;
+    var filteredOrders = _.groupBy(order.meals, 'meal');
+
+    var keys = _.keys(filteredOrders);
+
+    var groupedOrders = _.map(keys, function (mealName) {
+        var meals = filteredOrders[mealName];
+        var emails = meals.map(function(it) {
+            return {email: it.email, emailHash: CryptoJS.MD5(it.email.trim().toLowerCase())};
+            //return {email: it.email, emailHash: 'asdfasdfasdfasdf'};
+        });
+
+        return {mealName: mealName, count: meals.length, emails:emails};
     });
+
+    console.log(filteredOrders);
+    console.log("keys");
+    console.log(keys);
+
+    console.log(groupedOrders);
+
+    for (mealName in filteredOrders) {
+        if(filteredOrders.hasOwnProperty(mealName)) {
+
+        }
+    }
 
     var myOrder = order.meals.filter(function (it) {
         return it.id == mealId;
     });
 
-    return {order: order, filteredOrders: filteredOrders, myOrder: myOrder.length == 0 ? null : myOrder[0]};
+    return {order: order, groupedOrders: groupedOrders, myOrder: myOrder.length == 0 ? null : myOrder[0]};
 };
+
+Template.order.defaultEmail = function () {
+    return amplify.store("email");
+}
 
 Template.order.canOrderMeal = function () {
     return amplify.store(getMealIdName()) == undefined;
 }
 
+
 Template.order.events({
     'click #confirm': function (evnt, template) {
-        var mealId = Random.id();
+
         var email = template.find('#email').value;
         var meal = template.find('#meal').value;
-        var id = Session.get('orderId');
-        Orders.update({ _id: id }, {$push: {meals: {id: mealId, email: email, meal: meal}}});
-        Session.set('mealId', mealId);
 
-        amplify.store(getMealIdName(), mealId);
+        amplify.store("email", email);
+
+        addMeal(email, meal);
     },
     'click #remove': function (evnt, template) {
         var mealId = amplify.store(getMealIdName());
         var id = Session.get('orderId');
         Orders.update({ _id: id }, {$pull: {meals: {id: mealId}}});
         amplify.store(getMealIdName(), null);
+    },
+    'click #orderSame': function (evnt, template) {
+        var meal = Spark.getDataContext(evnt.target);
+        var email = amplify.store("email");
+
+        template.find('#meal').value = meal.mealName;
     }
 })
 
 function getMealIdName() {
     return Session.get('orderId') + '_mealId';
+}
+
+function addMeal(email, meal) {
+    var mealId = Random.id();
+    var id = Session.get('orderId');
+    Orders.update({ _id: id }, {$push: {meals: {id: mealId, email: email, meal: meal}}});
+    Session.set('mealId', mealId);
+
+    amplify.store(getMealIdName(), mealId);
 }
